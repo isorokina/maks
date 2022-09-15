38d30a2d993b6210cc9b773592e624d0479d2b62
isorokina <93764917+isorokina@users.noreply.github.com>
2022-09-15 15:35:32 +0300

    lab2

--- money.py
+++ money.py
@@ -4,16 +4,29 @@ class Purse:
         self.money=0.00
         self.valuta=valuta
         self.name=name
+    
     def top_up_balance(self, howmoney):
         self.money=self.money+howmoney
 
+    def top_down_balance(self, howmoney):
+        if self.money-howmoney<0:
+            print('Pietrūkst naudas makā')
+            raise ValueError ('Pietrūkst naudas makā')
+        self.money=self.money-howmoney
+
     def info(self):
-        return self.money
+        print(self.money)
+    
+    def __del__(self):
+        print('Maks izdzēsts')
+        #return self.money
 
 x=Purse('USD')
-y=Purse('EUR')
+y=Purse('EUR', 'Bill')
 x.top_up_balance(100)
-print(x.info())
+x.money=200
+x.info()
+
 
 #x.money=100
 #print(y.info())
